06ff19e96c1ce65d37666ae67ad25f2e5cf17837
LinaSyrota <92114653+LinaSyrota@users.noreply.github.com>
2022-11-18 18:19:35 +0200

    Add files via upload

---
+++ ООКП/Лаб 2/Models/PictureContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+
+namespace _Gallery_.Models
+{
+    public class PictureContext: DbContext
+    {
+        public PictureContext() : base("DefaultConnection")
+        { }
+
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
