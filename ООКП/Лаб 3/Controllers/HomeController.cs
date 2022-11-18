bd7a5d6517c7de4ea14cd6fdc7f06b0ad06c4305
LinaSyrota <92114653+LinaSyrota@users.noreply.github.com>
2022-11-18 22:04:38 +0200

    Add files via upload

---
+++ ООКП/Лаб 3/Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _Gallery_.Models;
+
+namespace _Gallery_.Controllers
+{
+    public class HomeController : Controller
+    {
+        PictureContext db = new PictureContext();
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewPictures(string PictureName, string sort)
+        {
+            IEnumerable<Picture> pictures = db.Pictures;
+
+            if (PictureName != "" && PictureName != null)
+            {
+                pictures = pictures.Where(x => x.Name == PictureName);
+            }
+
+            if (sort != null && sort != "")
+            {
+                if (sort == "Name")
+                    pictures = pictures.OrderBy(x => x.Name);
+                else if (sort == "Artist")
+                    pictures = pictures.OrderBy(x => x.Artist);
+            }
+
+            ViewBag.Pictures = pictures;
+
+            return View();
+        }
+
+        public ActionResult More(int id)
+        {
+            Picture thisPicture = db.Pictures.Find(id);
+
+            ViewBag.Id = id;
+            ViewBag.Name = thisPicture.Name;
+            ViewBag.Artist = thisPicture.Artist;
+            ViewBag.Genre = thisPicture.Genre;
+            ViewBag.Style = thisPicture.Style;
+            ViewBag.Materials = thisPicture.Materials;
+            ViewBag.Price = thisPicture.Price;
+            ViewBag.Link = thisPicture.Link;
+
+            return View();
+        }
+
+        public ActionResult Artist(int id)
+        {
+            IEnumerable<Artist> artists = db.Artists;
+
+            Picture thisPic = db.Pictures.Find(id);
+            foreach (var a in artists)
+            {
+                if (thisPic.Artist == a.ArtistsName)
+                {
+                    ViewBag.Artist = thisPic.Artist;
+                    ViewBag.Info = a.Info;
+                    ViewBag.Photo = a.Photo;
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.PictureId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+
+            foreach (var pic in db.Pictures)
+            {
+                if (pic.Id == purchase.PictureId)
+                {
+                    Picture thisPic = db.Pictures.Find(pic.Id);
+                    purchase.PicturesName = thisPic.Name;
+                }
+            }
+
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+
+            return "Операція виконана успішно!";
+        }
+
+
+        //[HttpGet]
+        //public ActionResult Search(string Search)
+        //{
+        //    ViewBag.search = Search;
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public void Search()
+        //{
+        //    //ViewPictures(ViewBag.search);
+        //}
+
+    }
+}
---
+++ ООКП/Лаб 3/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _Gallery_.Models
+{
+    [Table("Зображення")]
+    public class Image
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("Зображення")]
+        public string Link { get; set; }
+    }
+}
